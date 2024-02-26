564f3831a3e79c5a51882f52a98229e9e7a8f021
Matei Madalin <matei.madalin1@gmail.com>
2024-02-26 18:48:54 +0200

    Create db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+{
+  "books": [
+    { "id": 1, "title": "Book 1" },
+    { "id": 2, "title": "Book 2" },
+    { "id": 3, "title": "Book 3" }
+  ],
+  "students": [
+    { "id": 1, "name": "Student 1", "bookId": 1 },
+    { "id": 2, "name": "Student 2", "bookId": 1 }
+  ],
+  "profile": {
+    "name": "typicode"
+  }
+}
